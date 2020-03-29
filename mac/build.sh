6092839560ddbb9c8fc4b9ea26ede7a137fa4550
Ian Patterson <33583629+mortsnake@users.noreply.github.com>
2020-03-29 16:31:01 -0400

    Update build.sh

--- mac/build.sh
+++ mac/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -ex
+
+#Compile App
 mkdir -p VLC-protocol-app/Contents/MacOS
 gcc -framework Cocoa -o VLC-protocol-app/Contents/MacOS/vlc-protocol vlc-protocol.m
+
+#Add Icon File and Resource Directory 
+mkdir VLC-protocol-App/Contents/Resources
+curl -O https://mortis.moe/resources/siteicons/iconfile.icns
+mv iconfile.icns VLC-protocol-App/Contents/Resources/iconfile.icns
+
+curl -O GITHUB INFO.PLIST
