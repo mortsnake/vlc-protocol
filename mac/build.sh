bc3c4c9bfb16febfc5907df0239e4c76e4da01c3
Ian Patterson <33583629+mortsnake@users.noreply.github.com>
2020-03-29 17:00:13 -0400

    Update build.sh

--- mac/build.sh
+++ mac/build.sh
@@ -2,7 +2,14 @@
 
 #Compile App
 mkdir -p VLC-protocol-app/Contents/MacOS
-gcc -framework Cocoa -o VLC-protocol-app/Contents/MacOS/vlc-protocol vlc-protocol.m
+OLDVER=11
+CURVER=`sw_vers | grep ProductVersion | awk '{split($0, a, ":"); print a[2]}' | awk '{split($0, b, "."); print b[2]}'`
+if [ $CURVER -gt $OLDVER ]
+then
+  gcc -framework Cocoa -o VLC-protocol-app/Contents/MacOS/vlc-protocol vlc-protocol-10.14.m
+else
+  gcc -framework Cocoa -o VLC-protocol-app/Contents/MacOS/vlc-protocol vlc-protocol-10.11.m
+fi
 
 #Add Icon File and Resource Directory 
 mkdir VLC-protocol-App/Contents/Resources
